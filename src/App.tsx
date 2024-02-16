f5a7387f7a3f11eb32c5245de38a4b249cd867ac
omarkhalid12 <ok3817554@gmail.com>
2024-02-17 00:36:03 +0200

    custom modal componenet

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
+import { useState } from "react"
 import ProductCard from "./components/ProductCard"
+import Modal from "./components/UI/Modal"
 import { productList } from "./data"
+import Button from "./components/UI/Button"
 
 const App = () => {
-  //** Rendering
+  /* _______ STATE ________*/
+  const [isOpen, setIsOpen] = useState(false)
+
+  /* _______ HANDLER ________*/
+  function closeModal() {
+    setIsOpen(false)
+  }
+  function openModal() {
+    setIsOpen(true)
+  }
+  /* _______ RENDER ________*/
   const renderProductList = productList.map(product => <ProductCard key={product.id} product={product}/>)
   return (
     <main className="container">
+      <Button className="bg-indigo-700 hover:bg-indigo-800" onClick={openModal}>Add Product</Button>
       <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-2 gap-2 md:gap-4 rounded-md">
         {renderProductList}
       </div>
+
+      <Modal isOpen={isOpen} closeModal={closeModal} title="ADD A NEW PRODUCT">
+        <div className="flex items-center space-x-3">
+          <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
+          <Button className="bg-gray-300 hover:bg-gray-400">Cancel</Button>
+        </div>
+      </Modal>
     </main>
   )
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -27,8 +27,8 @@ const ProductCard = ({product} : IProps) => {
       </div>
 
       <div className="flex items-center justify-between space-x-2 mt-5">
-        <Button className="bg-indigo-700" width="w-full">Edit</Button>
-        <Button className="bg-red-700">Delete</Button>
+        <Button className="bg-indigo-700 hover:bg-indigo-800">Edit</Button>
+        <Button className="bg-red-700 hover:bg-red-800">Delete</Button>
       </div>
     </div>
   )
